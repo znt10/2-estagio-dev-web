fcbca2ab37f0e3bd8d3572bfbcbcc6f6b7f0b514
znt10 <jc970807@gmail.com>
2025-05-14 13:42:12 -0300

    dsds

--- meuprojeto/blog/urls.py
+++ meuprojeto/blog/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
-from .views import Post_ver, Post_DetailView, Post_CreateView, Post_UpdateView
+from .views import Post_ver, Post_DetailView, Post_CreateView, Post_UpdateView, Post_DeleteView
 
 urlpatterns = [
     path('', Post_ver.as_view(), name='postes'),
     path('ler/<int:pk>/', Post_DetailView.as_view(), name='ler_post'),
-    path('/criar/', Post_CreateView.as_view(), name='criar_post'),
-    path('/<int:pk>/editar', Post_UpdateView.as_view(), name='editar_post'),
-    path('/<int:pk>/deletar', Post_UpdateView.as_view(), name='deletar_post')
+    path('criar/', Post_CreateView.as_view(), name='criar_post'),
+    path('<int:pk>/editar/', Post_UpdateView.as_view(), name='editar_post'),
+    path('<int:pk>/deletar/', Post_DeleteView.as_view(), name='deletar_post'),
 ]
-
-
--- meuprojeto/blog/views.py
+++ meuprojeto/blog/views.py
@@ -1,12 +1,13 @@
-from django.views.generic import ListView,DeleteView,CreateView,UpdateView,DetailView
-from .models import Post  
+from django.views.generic import ListView, DeleteView, CreateView, UpdateView, DetailView
+from .models import Post
 from django.urls import reverse_lazy
+
 
 class Post_ver(ListView):
     model = Post
     template_name = 'blog/teste.html'
     context_object_name = 'posts'
-    paginate_by = 4
+    paginate_by = 6
 
 class Post_DetailView(DetailView):
     model = Post
@@ -16,11 +17,20 @@ class Post_DetailView(DetailView):
 class Post_CreateView(CreateView):
     model = Post
     template_name = 'blog/criar_Create.html'
-    fields = ['titulo', 'conteudo', 'imagem']
-    success_url = reverse_lazy('postes_criar')
-
+    fields = ['titulo', 'descricao', 'capa']  
+    success_url = reverse_lazy('postes')
+    def form_valid(self, form):
+        form.instance.autor = self.request.user
+        return super().form_valid(form)
+    
 class Post_UpdateView(UpdateView):
     model = Post
     template_name = 'blog/editar_Update.html'
-    fields = ['titulo', 'conteudo', 'imagem']
-    success_url = reverse_lazy('posts_editar')
+    fields = ['titulo', 'descricao', 'capa'] 
+    success_url = reverse_lazy('postes')
+
+class Post_DeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/deleta_Delete.html'
+    context_object_name = 'post_deletar'
+    success_url = reverse_lazy('postes')
