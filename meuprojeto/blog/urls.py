78df685c7889259bf814af309703521225901499
znt10 <jc970807@gmail.com>
2025-05-14 11:32:05 -0300

    certo

--- meuprojeto/blog/urls.py
+++ meuprojeto/blog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import Post_ver 
+from .views import Post_ver, Post_DetailView, Post_CreateView, Post_UpdateView
 
 urlpatterns = [
-    path('', Post_ver.as_view(), name='postes'), 
+    path('', Post_ver.as_view(), name='postes'),
+    path('ler/<int:pk>/', Post_DetailView.as_view(), name='ler_post'),
+    path('criar/', Post_CreateView.as_view(), name='criar_post'),
+    path('atualizar/<int:pk>/', Post_UpdateView.as_view(), name='atualizar_post'),
 ]
+
--- meuprojeto/blog/views.py
+++ meuprojeto/blog/views.py
@@ -1,7 +1,26 @@
-from django.views.generic import ListView
+from django.views.generic import ListView,DeleteView,CreateView,UpdateView,DetailView
 from .models import Post  
+from django.urls import reverse_lazy
 
 class Post_ver(ListView):
     model = Post
     template_name = 'blog/teste.html'
     context_object_name = 'posts'
+    paginate_by = 4
+
+class Post_DetailView(DetailView):
+    model = Post
+    template_name = 'blog/ler_detail.html'
+    context_object_name = 'post_detalhe'
+
+class Post_CreateView(CreateView):
+    model = Post
+    template_name = 'blog/criar_Create.html'
+    fields = ['titulo', 'conteudo', 'imagem']
+    success_url = reverse_lazy('postes_criar')
+
+class Post_UpdateView(UpdateView):
+    model = Post
+    template_name = 'blog/editar_Update.html'
+    fields = ['titulo', 'conteudo', 'imagem']
+    success_url = reverse_lazy('posts_editar')
