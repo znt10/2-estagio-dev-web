7437a18813dc0674f044a552b6e0c4b384f90839
znt10 <jc970807@gmail.com>
2025-05-14 11:34:19 -0300

    cc

--- meuprojeto/blog/urls.py
+++ meuprojeto/blog/urls.py
@@ -4,7 +4,8 @@ from .views import Post_ver, Post_DetailView, Post_CreateView, Post_UpdateView
 urlpatterns = [
     path('', Post_ver.as_view(), name='postes'),
     path('ler/<int:pk>/', Post_DetailView.as_view(), name='ler_post'),
-    path('criar/', Post_CreateView.as_view(), name='criar_post'),
-    path('atualizar/<int:pk>/', Post_UpdateView.as_view(), name='atualizar_post'),
+    path('/criar/', Post_CreateView.as_view(), name='criar_post'),
+    path('/<int:pk>/editar', Post_UpdateView.as_view(), name='editar_post'),
+    path('/<int:pk>/deletar', Post_UpdateView.as_view(), name='deletar_post')
 ]
 
