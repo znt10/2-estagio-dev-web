be7abde1f216cdcdd11f14bdf8e485ef8b742397
znt10 <jc970807@gmail.com>
2025-05-15 10:28:32 -0300

    certo

--- meuprojeto/blog/admin.py
+++ meuprojeto/blog/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-
 from .models import Post
 
 admin.site.register(Post)
+
+
+
+
+
+
--- meuprojeto/blog/models.py
+++ meuprojeto/blog/models.py
@@ -4,8 +4,10 @@ class Post(models.Model):
     titulo= models.CharField(max_length=200)
     descricao = models.TextField()
     capa = models.ImageField(upload_to='capas/',blank=True, null=True)
+    capa_extra = models.ImageField(upload_to='capas_extras/', blank=True, null=True)
     data_criacao = models.DateTimeField(auto_now_add=True)
     data_atualizacao = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.titulo
+
